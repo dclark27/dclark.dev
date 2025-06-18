820e9981a1435454ad3839bf2fedbf8acecb3433
Devin Clark <devin.clark27@gmail.com>
2025-06-18 10:26:18 -0500

    chore: initial next.js

---
+++ app/page.tsx
@@ -0,0 +1,51 @@
+export default function Page() {
+  return (
+    <section className="prose">
+      <h1>dclark.dev</h1>
+      <p>{"I'm devin clark. Web developer and tech enthusiast."}</p>
+
+      <span>apps</span>
+      <ul>
+        <li>
+          <a target="_blank" href="https://chex-one.vercel.app/login">
+            chex
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://fall-festival-karaoke.vercel.app/">
+            karaoke
+          </a>
+        </li>
+      </ul>
+      <br />
+      <span>socials</span>
+      <ul>
+        <li>
+          <a target="_blank" href="https://x.com/devin_clark">
+            twitter
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://bsky.app/profile/dclark.dev">
+            bsky
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://www.instagram.com/devin_clarkk/">
+            instagram
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://www.threads.net/@devin_clarkk">
+            threads
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://github.com/dclark27">
+            github
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
